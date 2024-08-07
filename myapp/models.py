318f5a7bc717dc288c20ff432cfa8d4747ff6a6f
JoelSantaM <joel.santa0199@gmail.com>
2024-08-06 20:47:55 -0500

    creando el server

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,3 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here. define la estructura de bd y sus relaciones
--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,7 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+# Create your views here. contiene la logica para manejar solicitudes http y manejar respuesta, interaccion del controlador
+
+def home(request):
+    return HttpResponse("Hola mmundo")
